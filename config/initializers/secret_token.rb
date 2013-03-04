68e3db953e00d6c6aabd607151c7148c8355bcfa
Salazar <salazar@voidrunners-VR-Box>
2013-03-04 11:29:50 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '61adc49d3803c4c077e63f62740c1fa276b9e432647b67d3431a6918e6f7c840de8348c9b82c8ec4bfc7628a6c2d07a5eed728a3469f86d303570278f796edd0'
